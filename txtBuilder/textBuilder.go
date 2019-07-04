e0de74c2469e55c4c61c88c1a5802ec3dd1140d7
adarshkumar <adarsh.kumar@thecarousell.com>
2019-07-04 23:03:17 +0800

    re struct code

--- txtBuilder/textBuilder.go
+++ txtBuilder/textBuilder.go
@@ -6,18 +6,17 @@ import (
 )
 
 func (b *TextBuilder) GetText() string {
-	if len(b.text.params) == 0 {
+	countTmplParam := strings.Count(b.text.template, "%v")
+	countParam := len(b.text.params)
+
+	if countTmplParam != countParam  || countParam == 0{
 		return b.text.template
 	}
+
 	values := []interface{}{}
 	for _,v := range b.text.params {
 		values = append(values, v)
 	}
 
-	countTmplParam := strings.Count(b.text.template, "%v")
-	if countTmplParam != len(b.text.params) {
-		return b.text.template
-	}
-	replacedText := fmt.Sprintf(b.text.template, values...)
-	return replacedText
+	return fmt.Sprintf(b.text.template, values...)
 }
